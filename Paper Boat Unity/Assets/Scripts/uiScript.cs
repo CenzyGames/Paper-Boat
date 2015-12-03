9b14ac61446d25eef210b0c607b93401400734e5
Shrey Kachhap <shreygodzi@gmail.com>
2015-12-03 11:11:21 +0530

    Ready for Ads Testing

--- Paper Boat Unity/Assets/Scripts/uiScript.cs
+++ Paper Boat Unity/Assets/Scripts/uiScript.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class uiScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Vector3 moveTo;
+    Vector3 moveBy;
+
+    void Start()
+    {
+        moveTo = Camera.main.transform.position;
+    }
+
+    void Update()
+    {
+        moveBy = Vector3.MoveTowards(Camera.main.transform.position, moveTo,1);
+        Camera.main.transform.position = moveBy;
+    }
+
+    public void playGame()
+    {
+
+    }
+
+    public void achievements()
+    {
+
+    }
+
+    public void showLeaderboard()
+    {
+
+    }
+
+    public void gotoPanel(float xVal)
+    {
+        moveTo = new Vector3(xVal, moveTo.y, moveTo.z);
+    }
+
+    public void buyItem(float value)
+    {
+
+    }
 }
---
+++ Paper Boat Unity/Assets/testScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<RectTransform>().localPosition += new Vector3(-1.0f, -1.0f, 0);
+	
+	}
+}
